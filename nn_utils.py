336cf50837efafb3c1f681780be706da77b38824
Brady Lowe <lowebra2@isu.edu>
2018-08-30 17:50:04 -0600

    Made code blocks easier to read and use

--- nn_utils.py
+++ nn_utils.py
@@ -3,82 +3,57 @@ import glob
 from scipy import misc
 from PIL import Image
 
-def load_dataset(samples=-1, shuffle=False, return_filenames=False):
-    
-    # Define training and testing data sizes
-    if samples != -1:
-        m_total = samples
-        m_train = int(m_total * 0.8)
-        m_test = m_total - m_train
+####################################################################################
+
+def get_label_from_filename(filename):
+    return int(filename[len(filename) - 5])
+
+####################################################################################
+
+def load_dataset(m=-1, return_filenames=False, im_dir="train"):
     
     # DEFINE IMAGE DIMENSIONS
     wpx, hpx = 87 * 4, 59 * 4
     
     # INITIALIZE STORAGE
-    train_pngs = []
-    test_pngs = []
-    train_labels = []
-    test_labels = []
-    train_filenames = []
-    test_filenames = []
+    images = []
+    labels = []
+    filenames = []
 
-    # Loop over all images in directory (up to m_train)
-    for image_path in glob.glob("images/train/*.png"):
+    # Loop over all images in directory (up to m)
+    for image_path in glob.glob("images/" + im_dir + "/*.png"):
         # Save the filename
-        train_filenames.append(image_path)
+        filenames.append(image_path)
         # Read in image to array
         cur_png = misc.imread(image_path)
-        # Resize image and append to list
-        train_pngs.append(misc.imresize(cur_png, (hpx, wpx)))
+        # Reduce resolution and append to list
+        images.append(misc.imresize(cur_png, (hpx, wpx)))
         # Grab the label off the end of the filename
-        train_labels.append(int(image_path[len(image_path) - 5]))
+        labels.append(get_label_from_filename(image_path))
         # Exit the loop if we hit the limit
-        if samples != -1 and len(train_labels) >= m_train:
-            break
-
-    # Loop over all images in directory (up to m_test)
-    for image_path in glob.glob("images/dev/*.png"):
-        # Save the filename
-        test_filenames.append(image_path)
-        # Read in image
-        cur_png = misc.imread(image_path)
-        # Resize image and append to list
-        test_pngs.append(misc.imresize(cur_png, (hpx, wpx)))
-        # Grab the label off the end of the filename
-        test_labels.append(int(image_path[len(image_path) - 5]))
-        # Exit the loop if we hit the limit
-        if samples != -1 and len(test_labels) >= m_test:
+        if m != -1 and len(labels) >= m:
             break
 
     # Convert all gathered info into arrays
-    train_x = np.asarray(train_pngs)
-    test_x = np.asarray(test_pngs)
-    train_y = np.asarray(train_labels)
-    test_y = np.asarray(test_labels)
+    images = np.asarray(images)
+    labels = np.asarray(labels)
     
     # Find out how many images were loaded
-    m_train = train_x.shape[0]
-    m_test = test_x.shape[0]
+    m = images.shape[0]
 
     # Invert and normalize images
-    train_x = (255. - train_x) / 255.
-    test_x = (255. - test_x) / 255.
+    images = (255. - images) / 255.
     
     # Reshape labels
-    train_y = train_y.reshape(train_y.shape[0], 1)
-    test_y = test_y.reshape(test_y.shape[0], 1)
+    labels = labels.reshape(labels.shape[0], 1)
 
-    # Shuffle
-    if (shuffle):
-        idx = np.random.permutation(m_train)
-        train_x = train_x[idx]
-        train_y = train_y[idx]
-        train_filenames = train_filenames[idx]
-        
+    # Return values
     if return_filenames:
-        return train_x, train_y, train_filenames, test_x, test_y, test_filenames
+        return images, labels, filenames
     else:
-        return train_x, train_y, test_x, test_y
+        return images, labels
+
+####################################################################################
 
 def get_mini_batches(X, Y, size, seed):
     np.random.seed(seed)
@@ -99,6 +74,8 @@ def get_mini_batches(X, Y, size, seed):
         batches.append(cur_batch)
     
     return batches
+
+####################################################################################
 
 def random_mini_batches(X, Y, batch_size, seed):
     np.random.seed(seed)
@@ -125,9 +102,81 @@ def random_mini_batches(X, Y, batch_size, seed):
     
     return batches
 
+####################################################################################
+
 def get_printable_image(this_image):
     this_image = 255 - this_image * 255
     this_image = misc.toimage(this_image)
     return this_image
+
+####################################################################################
+
+def get_stats(train_x, train_y, test_x = '', test_y = ''):
+    # Calculate predictions
+    predictions = conv_model.predict(x=train_x)
+    predictions = np.floor(predictions + 0.5)
+    # Get vector mask for good and bad fits and right and wrong answers
+    good_fits = train_y
+    bad_fits = 1 - good_fits
+    wrong_answers = np.abs(np.subtract(predictions, good_fits))
+    right_answers = np.subtract(1, wrong_answers)
+    # Calculate true positives (tp), as well as (tn) (fp) (fn)
+    true_positives = np.multiply(right_answers, good_fits)
+    true_negatives = np.multiply(right_answers, bad_fits)
+    false_positives = np.multiply(wrong_answers, good_fits)
+    false_negatives = np.multiply(wrong_answers, bad_fits)
+    # Calculate additional metrics
+    train_acc = np.sum(right_answers) / np.sum(right_answers + wrong_answers)
+    train_prec = np.sum(true_positives) / np.sum(true_positives + false_positives)
+    train_rec = np.sum(true_positives) / np.sum(true_positives + false_negatives)
+    if test_x == '':
+        return train_acc, train_prec, train_rec
+
+    # Do it again for testing set
+    predictions = conv_model.predict(x=test_x)
+    predictions = np.floor(predictions + 0.5)
+    good_fits = test_y
+    bad_fits = 1 - good_fits
+    wrong_answers = np.abs(np.subtract(predictions, good_fits))
+    right_answers = np.subtract(1, wrong_answers)
+    true_positives = np.multiply(right_answers, good_fits)
+    true_negatives = np.multiply(right_answers, bad_fits)
+    false_positives = np.multiply(wrong_answers, good_fits)
+    false_negatives = np.multiply(wrong_answers, bad_fits)
+    test_acc = np.sum(right_answers) / np.sum(right_answers + wrong_answers)
+    test_prec = np.sum(true_positives) / np.sum(true_positives + false_positives)
+    test_rec = np.sum(true_positives) / np.sum(true_positives + false_negatives)
+    
+    # Return all
+    return train_acc, train_prec, train_rec, test_acc, test_prec, test_rec
+
+####################################################################################
+
+def get_wrong_answers(x, y):
+    # Calculate predictions
+    predictions = conv_model.predict(x)
+    predictions = np.floor(predictions + 0.5)
+    # Get vector mask for good and bad fits and right and wrong answers
+    good_fits = y
+    wrong_answers = np.abs(np.subtract(predictions, good_fits))
+    # Make list of bad answers
+    bad_list = []
+    for i in range(len(y)):
+        if wrong_answers[i] == 1:
+            bad_list.append(i)
+    # Return list
+    return bad_list
+
+####################################################################################
+
+import os
+def change_label(filename):
+    # Get rid of old label and .png extension
+    new_name = filename[0:len(filename) - 5]
+    # Append new label and .png
+    new_label = 1 - get_label_from_filename(filename)
+    new_name += str(new_label) + ".png"
+    # Change the filename
+    os.rename(filename, new_name)
 
 
